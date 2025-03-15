3d518c5fc675cfc5259aa8d9805be3e90bc7656f
abasjy <abas13900621@gmail.com>
2025-03-15 10:21:27 +0330

    Add files via upload

---
+++ group.py
@@ -0,0 +1,14 @@
+from rubpy import filters, Client
+from rubpy.types import Update
+
+app = Client('h_o')
+
+@app.on_message_updates(filters.is_group)
+async def group(m:Update):
+    if m.text.startswith("e"):
+     t = m.text.replace('e','').strip()
+     up = sum(1 for char in t if char.isupper())
+     await m.reply(f"تعداد حروف بزرگ : {up} ")
+     
+     
+app.run()
